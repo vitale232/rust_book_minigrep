2ea0983c88231bbcd0d92f2dfa303f9b2455fe50
Andrew Vitale <avitale@vhb.com>
2022-02-19 12:44:22 -0500

    chore: initial commit mostly following https://doc.rust-lang.org/book/ch11-01-writing-tests.html#the-anatomy-of-a-test-function

---
+++ src/lib.rs
@@ -0,0 +1,114 @@
+use std::env;
+use std::error::Error;
+use std::fs;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents.lines().filter(|x| x.contains(query)).collect()
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    if results.len() > 0 {
+        for line in results {
+            println!("{}", line);
+        }
+    } else {
+        println!("No matches were found!");
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn creates_new_config() {
+        let config = Config::new(&[
+            String::from("program_name"),
+            String::from("what say you?"),
+            String::from("filename"),
+        ])
+        .expect("Failed to new!");
+        assert_eq!(
+            config,
+            Config {
+                query: String::from("what say you?"),
+                filename: String::from("filename"),
+                case_sensitive: false
+            }
+        );
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use rust_book_minigrep;
+use rust_book_minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("A problem occurred while parsing the arguments: {}", err);
+
+        process::exit(1);
+    });
+
+    if let Err(e) = rust_book_minigrep::run(config) {
+        eprintln!("An application error occurred! {}", e);
+
+        process::exit(1);
+    }
+}
